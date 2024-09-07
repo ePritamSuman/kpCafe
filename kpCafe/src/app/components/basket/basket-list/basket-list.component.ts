6eab0f134cd9c6054a41a7d58f9ad7d80b25afd5
Pritam Kumar Jha Suman <PritamKumarJha.Suman@viatris.com>
2024-09-08 00:29:04 +0530

    added few components

---
+++ kpCafe/src/app/components/basket/basket-list/basket-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './basket-list.component.html',
+  styleUrl: './basket-list.component.css'
+})
+export class BasketListComponent {
+
+}
---
+++ kpCafe/src/app/components/order/ordered-list/ordered-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ordered-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './ordered-list.component.html',
+  styleUrl: './ordered-list.component.css'
+})
+export class OrderedListComponent {
+
+}
